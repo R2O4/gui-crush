cdc225c42fe5798b8b95d6a0cc86ca707f589e10
R2O4 <86777288+R2O4@users.noreply.github.com>
2021-07-01 18:16:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Trần Quang Huân  Ơi!',
+    introTitle: 'Trần Quang Huân Ơi!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
